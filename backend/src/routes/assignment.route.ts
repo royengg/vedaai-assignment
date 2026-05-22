3f21e9667a1dc135e72ce31a928bc7afef06f8b9
Rudraksh Roy <gamermobile229@gmail.com>
2026-05-22 23:24:22 +0530

    init

---
+++ backend/src/routes/assignment.route.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { prisma } from "../lib/db";
+
+export const assignmentRouter = Router();
+
+assignmentRouter.post("/create", async (req, res) => {});
---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,60 @@
+import jwt from "jsonwebtoken";
+import { Router } from "express";
+import { prisma } from "../lib/db";
+import { registerSchema } from "../schemas/auth.schema";
+import { loginSchema } from "../schemas/auth.schema";
+import bcrypt from "bcrypt";
+import { JWT_SECRET } from "../lib/constants";
+
+export const authRouter = Router();
+
+authRouter.post("/login", async (req, res) => {
+  const parsed = loginSchema.safeParse(req.body);
+  if (!parsed.success) {
+    return res.status(400).json({ error: parsed.error });
+  }
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid Credentials" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ error: "Invalid Credentials" });
+    }
+    const token = jwt.sign({ id: user.id }, JWT_SECRET as string);
+    res.cookie("token", token);
+    return res.status(200).json({ user: user.name });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+authRouter.post("/register", async (req, res) => {
+  const parsed = registerSchema.safeParse(req.body);
+  if (!parsed.success) {
+    return res.status(400).json({ error: parsed.error });
+  }
+  const { email, name, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+    const token = jwt.sign({ id: user.id }, JWT_SECRET as string);
+    res.cookie("token", token);
+    return res.status(200).json({ user: user.name });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
